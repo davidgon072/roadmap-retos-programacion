47117fb355c30d3d4040c83868e25812b27d3818
davidgon072 <davidgon210607@gmail.com>
2025-03-09 16:28:57 -0500

    Create davidgon072.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/davidgon072.py
@@ -0,0 +1,12 @@
+# https://python.org
+
+# comentario en una linea
+
+my_variable = "Mi variable"
+my_variable = "Mi variable"
+
+MY_CONSTANT = "Mi constante"  # por convencion
+MY_CONTANT = "NHJNDSHJNFJN"
+
+
+
